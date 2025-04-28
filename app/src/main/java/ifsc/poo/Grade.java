459c14892e1bce1c6fbcde60b56595382150b14c
BrunoHamon <bruno.hp2002@aluno.ifsc.edu.br>
2025-04-28 09:20:48 -0300

    Grade

--- app/src/main/java/ifsc/poo/Grade.java
+++ app/src/main/java/ifsc/poo/Grade.java
@@ -6,10 +6,29 @@ public class Grade {
 
     private int posX;
     private int posY;
+    private int lado;
+    private int tamGrade;
+    private char rotY;
+    private int rotX;
 
     public Grade (Draw draw) {
-        this.posX = 30;
-        this.posY = 30;
-        draw.square(posX, posY, 10);
+        this.posX = 60;
+        this.posY = 60;
+        this.lado = 20;
+        this.tamGrade = 10;
+        for (int j = 0; j < this.tamGrade; j++) {
+            for (int i = 0; i < this.tamGrade; i++) {
+                draw.square(posX, posY, lado);
+                posX += 40;
+            }
+            posX = 60;
+            posY += 40;
+        }
+        int n = 0;
+        for (int i = 0; i < 100; i++)
+        draw.text(posX, posY, "" + rotY);
+        posX += 40;
+        n++;
+        this.rotY = (char) n;
     }
 }
